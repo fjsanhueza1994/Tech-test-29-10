d5423c39bdb2089ccb60a51f5403719bbcc76587
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-29 12:22:23 -0300

    + Create MusicSearchService with initial dependencies for project setup.

---
+++ MusicSearchService/src/test/java/com/tech/test/msmusicsearch/MusicSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.test.msmusicsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
