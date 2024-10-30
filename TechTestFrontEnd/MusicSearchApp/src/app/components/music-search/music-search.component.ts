9ebc49b9f8d4276ceb1517abbdeafa88d4e2e324
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-30 00:13:09 -0300

    ^ Implement music-search component in Angular to handle the display of track searches.

--- TechTestFrontEnd/MusicSearchApp/src/app/components/music-search/music-search.component.ts
+++ TechTestFrontEnd/MusicSearchApp/src/app/components/music-search/music-search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MusicSearchResponse} from "../../models/music-search-response.model";
+import {MusicSearchService} from "../../services/music-search.service";
 
 @Component({
   selector: 'app-music-search',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./music-search.component.css']
 })
 export class MusicSearchComponent {
+  artistName: string = '';
+  musicSearchResponse!: MusicSearchResponse;
+
+  constructor(private musicSearchService: MusicSearchService) {}
+
+  searchSongs(artistName: string): void {
+    this.musicSearchService.searchSongs(artistName).subscribe((response) => {
+      this.musicSearchResponse = response;
+    });
+  }
 
 }
