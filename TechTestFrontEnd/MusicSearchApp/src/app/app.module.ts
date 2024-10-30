fd29449420818f3746afd9a238280b0a65ab45e4
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-30 00:20:42 -0300

    ^ Add HttpClientModule and FormsModule to AppModule for API requests and form handling.

--- TechTestFrontEnd/MusicSearchApp/src/app/app.module.ts
+++ TechTestFrontEnd/MusicSearchApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +14,12 @@ import { MusicSearchComponent } from './components/music-search/music-search.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
