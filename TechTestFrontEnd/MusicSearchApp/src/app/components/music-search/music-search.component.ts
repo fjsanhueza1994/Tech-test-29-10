010cac067c9d10eeb8123f860972424e2132eb59
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-29 22:11:39 -0300

    + Add music search component.

---
+++ TechTestFrontEnd/MusicSearchApp/src/app/components/music-search/music-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-search',
+  templateUrl: './music-search.component.html',
+  styleUrls: ['./music-search.component.css']
+})
+export class MusicSearchComponent {
+
+}
