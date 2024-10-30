9d897d546d392516e369c7df6d448c32157fdb99
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-30 00:14:54 -0300

    ^ Define routing component properties.

--- TechTestFrontEnd/MusicSearchApp/src/app/app-routing.module.ts
+++ TechTestFrontEnd/MusicSearchApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MusicSearchComponent} from "./components/music-search/music-search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MusicSearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
